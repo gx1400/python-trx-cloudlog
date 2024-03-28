7019e421b7d40c51f5456167b661750ec71981b4
Derek Rowland <gx1400@gmail.com>
2024-03-28 12:19:34 -0500

    add override to stored message processing to ignore the min_interval check

--- cloudlog_reflector.py
+++ cloudlog_reflector.py
@@ -31,6 +31,7 @@ import websockets
 
 
 last_processed_times = {}
+last_stored_message = {}
 
 async def send_message(websocket, message):
     await websocket.send(message)
@@ -40,25 +41,26 @@ async def receive_response(websocket):
     response = await websocket.recv()
     print(f"Received response: {response}")
 
-async def process_message(websocket, message, min_interval):
+async def process_message(websocket, message, min_interval, override = False):
     global last_processed_times
-    current_time = time.time()
+    global last_stored_message
 
+    current_time = time.time()
     data = json.loads(message)
     msgtype = data.get('request', None)
-    radio = data.get('from')
+    
 
     if msgtype == "status-update":
+        radio = data.get('from')
         last_processed_time = last_processed_times.get(radio, 0)
 
-        # Check if enough time has elapsed since the last message
-        if current_time - last_processed_time >= min_interval:  
-            status = data.get('status',None)
+        if current_time - last_processed_time >= min_interval or override:  
+            status = data.get('status', None)
 
             if status is None:
                 return
 
-            radiofreq = status.get('frequency',None)
+            radiofreq = status.get('frequency', None)
             radiomode = status.get('mode', None)
 
             if radiofreq is None or radiomode is None:
@@ -76,11 +78,18 @@ async def process_message(websocket, message, min_interval):
             last_processed_times[radio] = current_time
         else:
             print(f"Ignoring message from {radio}: Too frequent")
+            last_stored_message[radio] = message  # Store the last received message
 
 async def receive_messages(websocket, min_interval):
     async for message in websocket:
         print(f"Received message from server: {message}")
         await process_message(websocket, message, min_interval)
+
+        # Check if there's a stored message for the radio and send it if the minimum interval has passed
+        radio = json.loads(message).get('from')
+        if radio in last_stored_message:
+            await process_message(websocket, last_stored_message.pop(radio), 
+                                  min_interval, True)
 
 def signal_handler(in_signal, frame):
     print('Keyboard interrupt! Exiting...')
@@ -134,6 +143,8 @@ async def main():
                 TimeoutError):
             print("Failed to connect. Retrying in 5 seconds...")
             await asyncio.sleep(5)
+
+    
 
 if __name__ == "__main__":
     asyncio.run(main())
